67238d795ab1f28253ea7496a46b371260ebbb0d
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2021-03-08 22:06:21 -0500

    added comments for all methods

--- jtar.cpp
+++ jtar.cpp
@@ -120,103 +120,6 @@ bool fileExist(string fileName)
 	return (test.is_open() ? true : false);
 }
 
-//call the stat command on a file with the "system" call and iterate through the information
-//that is stored from this 
-//precondition: pass in a string object that has the filename in it
-//postcondition: return a File object 
-/*File parseStat(string filename)
-{
-	string command = "stat " + filename + " > stats.txt";
-	fstream stat_file("stats.txt", ios::in);
-	system(command.c_str());
-	//variable to read in key words to find the data we want
-	string key_word;
-
-	//variable to store critical info for file class in
-	string file_info;
-	
-	//counter to track the number of times we see the "Access: " key_word
-	int times_access_seen;
-
-	//character arrays for our file object	
-	char name_array[81];
-	char size_array[7];
-	char pmode_array[5];
-	char stamp_array[16];
-	//while the file ptr is still alive, keep iterating through the file
-	while(stat_file)
-	{
-		stat_file >> key_word;
-		
-		if (key_word == "Access:")
-		{
-			times_access_seen += 1;
-			
-			getline(stat_file,file_info);
-			//call the method to get the protection mode
-			//
-			//method to pass in string and return c-string
-			if (times_access_seen == 1)
-			{ 
-				//get position of the ( in the string
-				size_t pos = file_info.find("("); 
-				string protection_mode = (file_info.substr(pos+1, 4));
-				
-				//get position of the / in the string
-				pos = file_info.find("/"); 
-				string file_or_dir = file_info.substr(pos+1,1);
-				
-				bool isDirectory = (file_or_dir == "d" ? true : false);
-				//copy the string with our file info into a char array
-				strcpy(pmode_array, protection_mode.c_str());
-				
-			}
-			//call the method to get the time stamp
-			//
-			//method that passes in a string and returns a c-string
-			else if (times_access_seen == 2)
-			{
-				int dash_pos = file_info.find("-");
-				int colon_pos = file_info.find(":");
-				string year = file_info.substr(0,dash_pos);
-
-				//dash_pos+1 is the starting place because we want it to 
-				//start the character after the dash
-				string month = file_info.substr(dash_pos+1,2);
-				
-				//dash_pos+4 because the the dash position includes the first
-				//dash (+1), then two characters for the month (+2), and 1 
-				//character for the second dash (+1)
-				string day = file_info.substr(dash_pos+4, 2);
-				string hour = file_info.substr(colon_pos-2, 2);
-				string minute = file_info.substr(colon_pos+1, 2);
-				string second = file_info.substr(colon_pos+4, 2);
-
-				string full_date = year+month+day+hour+minute+"."+second;
-				//copy the string with our full date into a char array
-				strcpy(stamp_array, full_date.c_str());
-			}
-		}
-		else if (key_word == "Size:")
-		{
-			stat_file >> file_info;
-			//copy the string with our file info into a char array
-			strcpy(size_array, file_info.c_str());
-		}
-		
-	}		
-	//copy the string with our file info into a char array
-	strcpy(name_array, filename.c_str());
-	
-	//instantiate a file object with the character arrays
-	File f(name_array, pmode_array, size_array, stamp_array);
-	
-	//remove the file.txt file from the directory
-	system("rm stats.txt");
-	return f;
-}*/
-
-
 //call the lstat command that will get the stat information for a file - this came
 //from Dr. Digh's utility.cpp file
 //precondition: pass in a string object that has the filename in it
@@ -234,14 +137,6 @@ File parseStat(string filename)
 	//this makes a call to our file being passed
 	lstat (filename.c_str(), &buf);
 
-	cout << filename << ", ";
-	cout << ", size = " << buf.st_size;
-	cout << ", protection = " << ((buf.st_mode & S_IRWXU) >> 6) << ((buf.st_mode & S_IRWXG) >> 3) << (buf.st_mode & S_IRWXO);
-
-	strftime(stamp, 16, "%Y%m%d%H%M.%S", localtime(&buf.st_mtime));
-
-	cout << ", timestamp = " << stamp << endl;
-		
 	//fill up the char arrays for the File object
 	
 	//copy the string with our filename into a char array
@@ -268,6 +163,8 @@ File parseStat(string filename)
 
 //method to split a string on the white space and put all words into a
 //vector of strings
+//precondition: pass in a string
+//postcondition: return a vector of strings 
 vector<string> splitOnWhiteSpace(string input)
 {
 	cout << "splitting on white space" << endl;
@@ -292,6 +189,9 @@ vector<string> splitOnWhiteSpace(string input)
 }
 
 //method to fill our tarfile (binary file)
+//precondition: pass in the file name for the tarfile (for creating an
+//fstream object) and a vector of Files that we are loading into the tarfile
+//postcondition: return nothing, but the tarfile is loaded with all the File objets
 void fillTarFile(string tarfile, vector<File>v)
 {
 	fstream outfile (tarfile, ios::out | ios::binary);
@@ -302,6 +202,8 @@ void fillTarFile(string tarfile, vector<File>v)
 }
 
 //method to convert a given integer to a string value
+//precondition: pass in an integer we want converted to a string
+//postcondition: return a string representation of the integer that was passed in
 string int_to_str(int input)
 {
 	stringstream s;
